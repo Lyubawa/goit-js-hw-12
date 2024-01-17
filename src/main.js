9dafc521b7fe8e6476c241044ca6c24098e36b38
Lyubawa <lyubawa81@ukr.net>
2024-01-17 09:16:34 +0200

    hw-js-12-3

--- src/main.js
+++ src/main.js
@@ -7,11 +7,15 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const form = document.querySelector(".form");
 const gallery = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
-const loadMoreBtn = document.querySelector('button[data-action="load-more"]');
+const loadMoreBtn = document.querySelector(".btn");
+const itemHeight = document.querySelector(".gallery-item");
 
-let searchQuery;
 
-const galleryqueryLightbox = new SimpleLightbox(".gallery a", {
+let query;
+let page = 1;
+const perPage = 40;
+
+const galleryLightbox = new SimpleLightbox(".gallery a", {
     captionsData: "alt",
     captionDelay: 250,
     nav: true,
@@ -20,18 +24,19 @@ const galleryqueryLightbox = new SimpleLightbox(".gallery a", {
     docClose: true,
 });
 
-
-async function searchImages(searchQuery) {
+async function searchImages(searchQuery, page) {
     const BASE_URL = "https://pixabay.com/api/";
     
     try {
-        const response = await api.get(`${BASE_URL}`, {
+        const response = await axios.get(`${BASE_URL}`, {
             params: {
     key: "41672793-a8580f18ed6f224a15f8d2674",
     q: searchQuery,
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
+    page: page,
+    per_page: perPage,
             }
         });
         return response.data;
@@ -44,33 +49,33 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
     gallery.innerHTML = '';
     loader.classList.remove('visible');
-    searchQuery = event.target.elements.search.value.trim();
-    if (!searchQuery) {
+    query = event.target.elements.search.value.trim();
+    if (!query) {
         iziToast.error({
-        title: "Error",
-        message: "Sorry, imput is empty!",
-        position: "topRight",
+            title: "Error",
+            message: "Sorry, imput is empty!",
+            position: "topRight",
         });
         loader.classList.add('visible');
         return;
     }
-    searchImages(searchQuery)
-    .then(({ hits }) => {
-        if (hits.length === 0) {
-            iziToast.error({
-        title: "Error",
-        message: "Sorry, there are no images matching your search query. Please try again!",
-        position: "topRight",
-        messageColor: "#ffffff",
-        titleColor: "#ffffff",
-        iconColor: "#ffffff",
-        backgroundColor: "#EF4040",
-            });   
-            return;
-            } 
-                const renderImages = hits.reduce((html, hit) => {
-                    return (
-                        html + `
+    searchImages(query)
+        .then(({ hits }) => {
+            if (hits.length === 0) {
+                iziToast.error({
+                    title: "Error",
+                    message: "Sorry, there are no images matching your search query. Please try again!",
+                    position: "topRight",
+                    messageColor: "#ffffff",
+                    titleColor: "#ffffff",
+                    iconColor: "#ffffff",
+                    backgroundColor: "#EF4040",
+                });
+                return;
+            }
+            const renderImages = hits.reduce((html, hit) => {
+                return (
+                    html + `
            <li class="gallery-item">
            <a class="gallery-link" href="${hit.largeImageURL}">
            <img
@@ -87,18 +92,90 @@ form.addEventListener("submit", (event) => {
            </div>
            </li>
             `
-            );
+                );
             }, "")
             gallery.innerHTML = renderImages;
             galleryLightbox.refresh();
-               })        
+        })
         .catch((error) => console.log(error))
         .finally(() => {
-        loader.classList.add('visible');
+            loader.classList.add('visible');
         });
     event.currentTarget.reset();
-})
+});
 
+page = 1;
+
+loadMoreBtn.addEventListener("click", () => {
+    try {
+        page += 1;
+        loadMoreBtn.classList.add('visible');
+        loader.classList.remove('visible');
+
+        itemHeight.getBoundingClientRect().height;
+
+        searchImages(query, page)
+            .then((response) => {
+                renderImages(response.data.hits);
+
+                window.scrollBy(0, itemHeight * 2);
+
+                const totalPages = Math.ceil(response.data.totalHits / perPage);
+
+                if (page === totalPages) {
+                    loadMoreBtn.classList.remove('visible');
+                    iziToast.show({
+                        message: "We're sorry, but you've reached the end od search results",
+                        position: "topRight",
+                    });
+                    loader.classList.add('visible');
+                    return;
+                }
+                galleryLightbox.refresh();
+
+                loadMoreBtn.classList.remove('visible');
+                loader.classList.remove('visible');
+            });
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+// searchImages(query, page) 
+//     .then(response => {
+//         if (!response.data.hits.length) {
+//             iziToast.error({
+//                     title: "Error",
+//                     message: "Sorry, there are no images matching your search query. Please try again!",
+//                     position: "topRight",
+//                     messageColor: "#ffffff",
+//                     titleColor: "#ffffff",
+//                     iconColor: "#ffffff",
+//                     backgroundColor: "#EF4040",
+//                 });
+//         }
+//         else if (response.data.hits.length > perPage) {
+//             iziToast.show({
+//                 message: "We're sorry, but you've reached the end od search results",
+//                 position: "topRight",
+//             });
+//         }
+//         renderImages(response.data.hits);
+//         galleryLightbox.refresh();
+//         form.reset();
+//     })
+//     .catch((error) => console.log(error))
 
 
 
